809dc35261f8d736f950f8b9b833963ca255a299
tcousik <119012703+tcousik@users.noreply.github.com>
2023-10-20 17:12:36 -0600

    modal

--- src/components/home/BookSingleCard.jsx
+++ src/components/home/BookSingleCard.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 import { PiBookOpenTextLight } from 'react-icons/pi'
-import { BiUserCircle } from 'react-icons/bi';
+import { BiUserCircle, BiShow } from 'react-icons/bi';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineDelete } from 'react-icons/md';
+import { useState } from 'react';
+import BookModal from './BookModal';
 
 const BookSingleCard = ({ book }) => {
+    const [showModal, setShowModal] = useState(false);
     return (
         <div
             key={book._id}
@@ -22,6 +25,10 @@ const BookSingleCard = ({ book }) => {
                 <h2 className='my-1'>{book.author}</h2>
             </div>
             <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
+                <BiShow
+                    className='text-3xl text-blue-800 hover:text-black cursor-pointer'
+                    onClick={() => setShowModal(true)}
+                />
                 <Link to={`/books/details/${book._id}`}>
                     <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
                 </Link>
@@ -32,6 +39,11 @@ const BookSingleCard = ({ book }) => {
                     <MdOutlineDelete className='text-2xl text-red-600 hover:text-black' />
                 </Link>
             </div>
+            {
+                showModal && (
+                    <BookModal book={book} onClose={() => setShowModal(false)} />
+                )
+            }
         </div>
     )
 }
